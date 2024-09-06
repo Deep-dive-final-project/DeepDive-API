39937810c97beecdc995cc4b83860e7c63d3f0b4
ChangHee98 <dlackdgml2@naver.com>
2024-09-07 01:23:04 +0900

    :hammer: [Fix] lecture @Param 오류 해결

--- src/main/java/org/deepdive/apiserver/lecture/repository/jpa/JpaMyLectureQueryRepository.java
+++ src/main/java/org/deepdive/apiserver/lecture/repository/jpa/JpaMyLectureQueryRepository.java
@@ -4,11 +4,12 @@ import java.util.List;
 import org.deepdive.apiserver.lecture.repository.entity.MyLectureEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface JpaMyLectureQueryRepository extends JpaRepository<MyLectureEntity, Long> {
 
     @Query("select ml from MyLectureEntity ml"
         + " join fetch ml.member m"
         + " where m.memberId=:memberId")
-    List<MyLectureEntity> findAllByMemberId(Long memberId);
+    List<MyLectureEntity> findAllByMemberId(@Param("memberId") Long memberId);
 }
