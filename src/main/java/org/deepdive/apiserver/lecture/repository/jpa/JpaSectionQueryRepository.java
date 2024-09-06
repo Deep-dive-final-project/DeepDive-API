7f937912a77ed0cc293a0edc7b139049207fab60
ChangHee98 <dlackdgml2@naver.com>
2024-09-07 02:06:28 +0900

    :hammer: [Fix] lecture findAllByLectureId @Param 오류 해결

--- src/main/java/org/deepdive/apiserver/lecture/repository/jpa/JpaSectionQueryRepository.java
+++ src/main/java/org/deepdive/apiserver/lecture/repository/jpa/JpaSectionQueryRepository.java
@@ -4,11 +4,12 @@ import java.util.List;
 import org.deepdive.apiserver.lecture.repository.entity.SectionEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface JpaSectionQueryRepository extends JpaRepository<SectionEntity, Long> {
 
     @Query("select s from SectionEntity  s"
         + " join fetch s.lecture l"
         + " where l.lectureId=:lectureId")
-    List<SectionEntity> findAllByLectureId(Long lectureId);
+    List<SectionEntity> findAllByLectureId(@Param("lectureId") Long lectureId);
 }
