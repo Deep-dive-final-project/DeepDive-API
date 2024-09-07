6b09f52548256bb11a89d5e0ecb8dc14f51625da
ChangHee98 <dlackdgml2@naver.com>
2024-09-08 02:14:07 +0900

    :hammer: [Refactor] note 로직에 task 추가

--- src/main/java/org/deepdive/apiserver/note/domain/Note.java
+++ src/main/java/org/deepdive/apiserver/note/domain/Note.java
@@ -2,6 +2,7 @@ package org.deepdive.apiserver.note.domain;
 
 import lombok.Builder;
 import lombok.Getter;
+import org.deepdive.apiserver.plan.domain.Task;
 import org.deepdive.apiserver.security.domain.Member;
 
 @Getter
@@ -9,13 +10,15 @@ import org.deepdive.apiserver.security.domain.Member;
 public class Note {
     private Long noteId;
     private Member member;
+    private Task task;
     private String title;
     private String content;
     private String summary;
 
-    public static Note createNote(Member member, String title, String content, String summary){
+    public static Note createNote(Member member, Task task, String title, String content, String summary){
         return Note.builder()
                 .member(member)
+                .task(task)
                 .title(title)
                 .content(content)
                 .summary(summary)
