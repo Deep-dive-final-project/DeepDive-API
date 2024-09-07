e07c2afde0d47c7177ad588101896824e4d94d80
ChangHee98 <dlackdgml2@naver.com>
2024-09-08 01:39:55 +0900

    :sparkles: [Feat] Task 로직에 필요한 클래스 구현

---
+++ src/main/java/org/deepdive/apiserver/plan/application/interfaces/TaskRepository.java
@@ -0,0 +1,5 @@
+package org.deepdive.apiserver.plan.application.interfaces;
+
+
+public interface TaskRepository {
+}
---
+++ src/main/java/org/deepdive/apiserver/plan/domain/Task.java
@@ -0,0 +1,20 @@
+package org.deepdive.apiserver.plan.domain;
+
+import java.sql.Timestamp;
+import lombok.Builder;
+import lombok.Getter;
+import org.deepdive.apiserver.lecture.domain.section.Section;
+import org.deepdive.apiserver.security.domain.Member;
+
+@Builder
+@Getter
+public class Task {
+
+    private Long taskId;
+    private Plan plan;
+    private Section section;
+    private Member member;
+    private String title;
+    private String state;
+    private Timestamp completeDate;
+}
