a2c73409e573d79a72e2bdd7c37c6250b77a9ad5
koosco <rnxogud123@naver.com>
2024-09-10 01:32:37 +0900

    [feat] jwt errorcode 추가

--- src/main/java/org/deepdive/apiserver/common/exception/ErrorCode.java
+++ src/main/java/org/deepdive/apiserver/common/exception/ErrorCode.java
@@ -47,6 +47,16 @@ public enum ErrorCode {
     ACCESS_DENIED_PLAN(40304, HttpStatus.BAD_REQUEST, "학습 계획에 접근할 수 없습니다."),
     ACCESS_DENIED_TASK(40305, HttpStatus.BAD_REQUEST, "태스크에 접근할 수 없습니다."),
 
+    // Unauthorized Error
+    FAILURE_LOGIN(40100, HttpStatus.UNAUTHORIZED, "잘못된 아이디 또는 비밀번호입니다."),
+    EXPIRED_TOKEN_ERROR(40101, HttpStatus.UNAUTHORIZED, "만료된 토큰입니다."),
+    INVALID_TOKEN_ERROR(40102, HttpStatus.UNAUTHORIZED, "유효하지 않은 토큰입니다."),
+    TOKEN_MALFORMED_ERROR(40103, HttpStatus.UNAUTHORIZED, "토큰이 올바르지 않습니다."),
+    TOKEN_TYPE_ERROR(40104, HttpStatus.UNAUTHORIZED, "토큰 타입이 일치하지 않거나 비어있습니다."),
+    TOKEN_UNSUPPORTED_ERROR(40105, HttpStatus.UNAUTHORIZED, "지원하지않는 토큰입니다."),
+    TOKEN_GENERATION_ERROR(40106, HttpStatus.UNAUTHORIZED, "토큰 생성에 실패하였습니다."),
+    TOKEN_UNKNOWN_ERROR(40107, HttpStatus.UNAUTHORIZED, "알 수 없는 토큰입니다."),
+    EXPIRED_REFRESH_TOKEN_ERROR(40108, HttpStatus.UNAUTHORIZED, "만료된 토큰입니다."),
 
     INTERNAL_SERVER_ERROR(50000, HttpStatus.INTERNAL_SERVER_ERROR, "서버 내부 에러입니다."),
     UPLOAD_FILE_ERROR(50001, HttpStatus.INTERNAL_SERVER_ERROR, "파일 업로드에 실패하였습니다."),
