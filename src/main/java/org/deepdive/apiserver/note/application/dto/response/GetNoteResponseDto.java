8691ac5f149503395c257b86cc4d61eaed624719
ChangHee98 <dlackdgml2@naver.com>
2024-09-11 01:46:54 +0900

    :hammers: [Refactor] Note 강의 노트 조회 title 추가

--- src/main/java/org/deepdive/apiserver/note/application/dto/response/GetNoteResponseDto.java
+++ src/main/java/org/deepdive/apiserver/note/application/dto/response/GetNoteResponseDto.java
@@ -5,11 +5,13 @@ import org.deepdive.apiserver.note.domain.Note;
 
 @Builder
 public record GetNoteResponseDto(
+        String title,
         String content,
         String summary
 ) {
     public static GetNoteResponseDto fromEntity(Note note){
         return GetNoteResponseDto.builder()
+                .title(note.getTitle())
                 .content(note.getContent())
                 .summary(note.getSummary())
                 .build();
